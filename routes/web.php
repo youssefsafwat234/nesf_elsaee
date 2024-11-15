f9347c98326fa7a2a6a268e2b9188f63a666c0fc
youssefsafwat234 <yousefsafwat367@gmail.com>
2024-11-15 11:42:06 +0200

    change the  logic of the register

--- routes/web.php
+++ routes/web.php
@@ -15,40 +15,7 @@ use Illuminate\Support\Facades\Route;
 //
 //
 Route::get('test', function () {
-// تحديد النطاق الفرعي بناءً على البيئة
-   $subdomain = config('app.env') === 'production' ? 'iamservices' : 'test-iamservices';
-
-   // إعداد رابط API
-   $url = "https://test-iamservices.semati.sa/nafath/api/v1/client/authorize/";
-
-   // إعداد عميل Guzzle
-   $client = new \GuzzleHttp\Client();
-
-   try {
-       // إرسال الطلب
-       $response = $client->post($url, [
-           'headers' => [
-               "id" => "1000062537",
-               'Content-Type' => 'application/json',
-               'Authorization' => "apikey 8f4b3d9a-e931-478d-a994-28a725159ab9",
-               "action" => "SpRequest",
-               "service" => "Login"
-
-           ],
-           'timeout' => 120
-       ]);
-
-       // معالجة الاستجابة
-       $data = json_decode($response->getBody()->getContents(), true);
-
-       return response()->json($data);
-   } catch (\Exception $e) {
-       // التعامل مع الأخطاء
-       return response()->json(['error' => $e->getMessage()], 500);
-   }
-
-
-
+    dd(\App\Models\User::all());
 });
 
 
